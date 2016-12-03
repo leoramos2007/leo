acfce2fd2157b59728eaa1191a4bdf0c41f63733
leoramos2007 <leo.ramospereira@hotmail.com>
2016-12-03 00:51:13 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,57 +1,62 @@
 <?php
 
-//Código em html para montar o form.
-
-echo "
-<html>
-<head>
-<title>IMC</title>
-</head>
-<body>
-   Cálculo do Índice de Massa Corporal.<p>
-  <form action=\"imc2.php\" method=\"post\" name=\"f1\">
- Peso:<input name=\"peso\" type=\"text\" size=\"10\" maxlength=\"50\" /><p>
- Altura:<input name=\"altura\" type=\"text\" size=\"10\" maxlength=\"50\" /><p>
- <input name=\"enviar\" type=\"submit\" value=\"OK\" />
- </form>
-
-</body>
-</html>";
 ?>
 
-Crie o arquivo imc2.php com o código abaixo:
-
+<html>
+<form name="form1" id="form1" method="POST" action="index.php">
+Altura: <input name="altura" type="text" id="altura" size="10" maxlength="10"> 
+<br>
+Peso: <input name="peso" type="text" id="peso" size="12" maxlength="152"> 
+<br>
+<input name="Submit" type="submit" id="calcular" value="Calcular">
+<br>
+<br>
+usar somente ponto
+</form>
+<br>
+<br>
+<br>
 <?php
+$altura = $_POST["altura"];
+$peso = $_POST["peso"];
 
-// Obtém as variáveis vindas do formulário.
-
-$peso = $_POST['peso'];
-$altura = $_POST['altura'];
-
-$imc=$peso/($altura*$altura);
-
-echo "
-<html>
-<head>
-<title>IMC</title>
-</head>
-<body>
-$imc<p>";
- 
-  if ($imc<18.5){ 
- echo " Peso abaixo do Normal !"; 
-  } else if (($imc > 18.5) && ($imc < 24.49)){
- echo " Peso Normal !";  
-  } else if (($imc > 24.5) && ($imc < 29.9)){
- echo " Pré-Obesidade !";  
-  } else if (($imc > 30) && ($imc < 34.9)){
- echo " Obesidade classe I !";  
-  } else if (($imc > 35) && ($imc < 39.9)){
- echo " Obesidade classe II !";   
- } else if ($imc > 40){
- echo " Obesidade classe III !";   
- }
-  echo "
-</body>
-</html>";
+$imc = $peso / ($altura * $altura);
 ?>
+<br>
+<? 
+if ($imc < 17){
+$msg = "Muito abaixo do peso!";
+
+
+}elseif (($imc >= 17)  &&($imc <= 18.49)){
+$msg = "Abaixo do peso";
+
+
+}elseif (($imc >= 18.5)&&($imc <= 24.99)){
+$msg = "Peso normal";
+
+
+}elseif (($imc >= 25)  &&($imc <= 29.99)){
+$msg = "Acima do peso";
+
+
+}elseif (($imc >= 30)  &&($imc <= 34.99)){
+$msg = "Obesidade I";
+
+
+}elseif (($imc >= 35)  &&($imc <= 39.99)){
+$msg = "Obesidade II (severa)";
+
+
+}elseif ($imc > 40){
+$msg = "Obesidade III (mórbida)";
+
+}
+
+?>
+<b>IMC: </b><?php echo $imc; ?>
+<br>
+<b>Mensagem: </b><?php echo $msg; ?>
+<br>
+<?php echo $img; ?>
+</html>
